c963334a21bb045fe40ad4390071a49069cd73b5
lerickl <nearerick@gmail.com>
2023-11-13 16:11:53 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,26 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-
-import Link from 'next/link';
+ 
+import AcmeLogo from '@/app/ui/acme-logo'
+import DashboardSkeleton, { CardsSkeleton, RevenueChartSkeleton } from './ui/skeletons'
+import Link from 'next/link'
+import { InvoiceSkeleton, LatestInvoicesSkeleton } from './ui/skeletons'
+import styles from './ui/home.module.css'
+import { useState } from 'react'
+import InvoiceStatus from './ui/invoices/status'
+import { lusitana } from '@/app/ui/fonts'
+import Image from 'next/image'
 export default function Page() {
+ 
+  const shimmer =
+  'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
+ 
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+        <AcmeLogo /> 
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -23,8 +34,31 @@ export default function Page() {
             <span>Log in</span>
           </Link>
         </div>
+        <div  > 
+      
+        <div
+          className={styles.shape}
+        
+        />
+        
+        </div>
+        <InvoiceStatus status={'paid'}/>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop and mobile versions"></Image>
+
         </div>
       </div>
     </main>
